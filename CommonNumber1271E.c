ab1ed601ad56b5b5e4b0412ed78b99a4a382267b
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-19 22:51:53 +0100

    Developing the common number function

--- CommonNumber1271E.c
+++ CommonNumber1271E.c
@@ -3,21 +3,33 @@
 //
 /*
  * https://codeforces.com/problemset/problem/1271/E
- *
  */
 
 #include "CommonNumber1271E.h"
 
+uint32_t max(const uint32_t pInt[], uint32_t n, uint32_t idx) {
+    uint32_t max = pInt[0];
+    for (uint32_t i = 0; i < n; ++i) {
+        if (max < pInt[i])
+            max = pInt[i];
+    }
+    return max;
+}
+
 uint32_t commonNumber(uint32_t pathsNumber, uint32_t k) {
-    uint32_t temp = pathsNumber, n = 0, tab[pathsNumber];
-    (temp % 2 == 0) ? (n = pathsNumber/2 + 1) : (n = pathsNumber/2 + 2);
-    uint32_t i = 0;
+    uint32_t tab[pathsNumber], temp = 0, length = pathsNumber;
+    tab[pathsNumber] = 1;
+    begin:
+    temp = pathsNumber;
     while (temp != 1) {
         (temp % 2 == 0) ? temp /= 2 : temp--;
         tab[temp]++;
-        i++;
     }
-
-
-    return 0;
+    pathsNumber--;
+    if (pathsNumber == 1) return max(tab, length, k);
+    goto begin;
 }
+
+
+
+
--- CommonNumber1271E.h
+++ CommonNumber1271E.h
@@ -7,6 +7,10 @@
 
 #include <stdint.h>
 
-extern uint32_t commonNumber(uint32_t , uint32_t);
+/* The max function is designed to calculate the max of the table pInt whose length is n */
+extern uint32_t max(const uint32_t pInt[], uint32_t n, uint32_t idx);
+
+/* The commonNumber function returns the maximum common number between all paths */
+extern uint32_t commonNumber(uint32_t pathNumber, uint32_t idx);
 
 #endif //CODEFORCES_COMMONNUMBER1271E_H
