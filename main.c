b7a437be1a32fb2a0c4d80db837bf0e48b37a282
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-20 22:33:36 +0100

    Test 4: Failed (1e+6, 100) = ?? program crashed

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main() {
     /* First argument of the CommonFunction must be >= of the second argument*/
-    uint32_t d = commonNumber(1000000, 100);
+    uint32_t d = commonNumber(10000, 400);
     printf("d = %d\n", d);
     return 0;
 }
